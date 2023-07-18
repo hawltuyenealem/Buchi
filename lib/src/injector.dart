07f700f8c64d1351eacd946297da1c375d3764bb
hawltu yenealem <haweltu@mohastech.com>
2023-07-18 23:45:44 +0300

    add pet use case

--- lib/src/injector.dart
+++ lib/src/injector.dart
@@ -3,6 +3,7 @@
 import 'package:buchi/src/data/datasources/pet_datasource.dart';
 import 'package:buchi/src/data/repositories/pet_repo_impl.dart';
 import 'package:buchi/src/domain/repository/pet_repo.dart';
+import 'package:buchi/src/domain/usecase/pet_usecase.dart';
 import 'package:buchi/src/presentation/pets/bloc/pet_bloc.dart';
 import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
@@ -16,6 +17,8 @@ Future<void> serviceLocatorInit() async {
   /// DATASOURCES
   sl.registerFactory<PetDataSource>(() =>PetDataSource());
 
+   /// USECASE
+  sl.registerLazySingleton<PetUsecase>(() => PetUsecase(petRepo: sl()));
 
   ///REPOSITORIES
   sl.registerLazySingleton<PetRepo>(() =>PetRepoImpl(sl(),sl()));
